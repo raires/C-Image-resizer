33c09252a8bf2873c7fceb8ae0fabee7e3e4ac3a
Rodrigo Aires <rodrigo.aires@gmail.com>
2017-03-19 22:45:29 -0400

    cleaned files

--- ResizeMaxWH/Properties/AssemblyInfo.cs
+++ ResizeMaxWH/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ResizeMaxWH")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ResizeMaxWH")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
